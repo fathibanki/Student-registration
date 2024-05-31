aef4729718f283de84361c6b8660ef88382317a3
duresaobsaa <duresaobsa1@gmail.com>
2024-06-01 01:17:19 +0300

    student.sql

---
+++ student.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 31, 2024 at 09:43 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `cid` int(10) NOT NULL,
+  `course_name` varchar(30) NOT NULL,
+  `dept` varchar(30) NOT NULL,
+  `maxlimit` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`cid`, `course_name`, `dept`, `maxlimit`) VALUES
+(1, 'MTECH', 'CS', 2),
+(2, 'MTECH', 'IS', 16),
+(3, 'MTECH', 'IT', 16),
+(4, 'ss2028', 'Economics', 16),
+(5, 'ss2032', 'Economics', 16),
+(6, 'ss2030', 'ITM', 60),
+(7, 'MD44', 'Medicine', 2),
+(8, 'pc77', 'pharmacy', 60);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hostel`
+--
+
+CREATE TABLE `hostel` (
+  `hostel_id` int(10) NOT NULL,
+  `hostel_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_details`
+--
+
+CREATE TABLE `image_details` (
+  `image_id` tinyint(3) NOT NULL DEFAULT 0,
+  `image_type` varchar(25) NOT NULL DEFAULT '',
+  `image` blob NOT NULL,
+  `image_size` varchar(25) NOT NULL DEFAULT '',
+  `image_ctgy` varchar(25) NOT NULL DEFAULT '',
+  `image_name` varchar(50) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_details`
+--
+
+CREATE TABLE `student_details` (
+  `eid` varchar(7) NOT NULL,
+  `rno` int(3) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `dob` date NOT NULL,
+  `fathername` varchar(30) NOT NULL,
+  `hostelid` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `sem` int(2) NOT NULL,
+  `cid` int(10) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `gender` varchar(7) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT 'MALE',
+  `nationality` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `student_details`
+--
+
+INSERT INTO `student_details` (`eid`, `rno`, `name`, `phone`, `email`, `address`, `dob`, `fathername`, `hostelid`, `password`, `sem`, `cid`, `image`, `gender`, `nationality`) VALUES
+('1206114', 2, 'FathiBanki', '251986323280', 'fatihbankii@gmail.com', 'chiro', '2024-05-08', 'banki', '1206114', '12345678', 5, 3, '', 'female', 'Indian'),
+('1206115', 48, 'fathibanki', '25198632328', 'kd@gmail.com', 'chiro', '2001-07-26', 'banki', '1206115', '12345678', 5, 1, '', 'male', 'Indian'),
+('1206116', 10, 'fenet kasaye', '251986323286', 'dude@gmail.com', 'chiro', '2024-05-01', 'banki', '1206116', 'password', 3, 4, '', 'female', 'NRI'),
+('1206117', 48, 'fenetfati', '251986323280', 'fatihbankii1@gmail.com', 'chiro', '2002-06-11', 'banki', '1206116', 'password1', 7, 6, '', 'female', 'NRI'),
+('1206118', 914, 'fenet kasayee', '8769873087078', 'Gaddisa914@gmail.com', 'chiro', '2024-05-09', 'banki', '914', 'qwertyuiop', 3, 5, '', 'female', 'Indian'),
+('134565', 58, 'brobrobro', '1234567889', 'kid@gmail.com', 'adama', '2024-05-02', 'bankiii', '914', 'asdfghjkl', 3, 7, '', 'female', 'Indian'),
+('305018', 18, 'refikaaliyyi', '251938095018', 'refika@gmail.com', 'ambo', '2003-02-20', 'fathi', '5018', '38095018', 1, 8, '', 'female', 'NRI');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `student_details`
+--
+ALTER TABLE `student_details`
+  ADD PRIMARY KEY (`eid`),
+  ADD UNIQUE KEY `eid` (`eid`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `email_2` (`email`),
+  ADD UNIQUE KEY `email_3` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
